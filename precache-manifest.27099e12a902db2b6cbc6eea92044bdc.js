75ddaa65aabf6e91904e639a7d8e616f568b46e5
Camilo <peronacamilo@gmail.com>
2020-08-08 21:32:58 -0300

    Updates

---
+++ precache-manifest.27099e12a902db2b6cbc6eea92044bdc.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb8f3d00a06ab31b69365a3f9872d8a0",
+    "url": "/web-portfolio/index.html"
+  },
+  {
+    "revision": "76a5c9ce38799e53e09d",
+    "url": "/web-portfolio/static/css/main.76975645.chunk.css"
+  },
+  {
+    "revision": "73a6b24f10fbf5915f7f",
+    "url": "/web-portfolio/static/js/2.6208e7cb.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/web-portfolio/static/js/2.6208e7cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76a5c9ce38799e53e09d",
+    "url": "/web-portfolio/static/js/main.7124e85e.chunk.js"
+  },
+  {
+    "revision": "b4ef9ed81d43616385f1",
+    "url": "/web-portfolio/static/js/runtime-main.9e2f8706.js"
+  },
+  {
+    "revision": "c7b8b1244f2756d40dde3dca94dfaa46",
+    "url": "/web-portfolio/static/media/Detalle-Eventos-desktop.c7b8b124.png"
+  },
+  {
+    "revision": "f90c26a1f0c9fd5375f0c46494801aa3",
+    "url": "/web-portfolio/static/media/Landing 01 - Sancor-01.f90c26a1.png"
+  },
+  {
+    "revision": "eaeb2f2f73effd02f08d34b87d6731ff",
+    "url": "/web-portfolio/static/media/Landing 01 - Sancor-02.eaeb2f2f.png"
+  },
+  {
+    "revision": "27e3649bab7c62fa21b8837c4842e40e",
+    "url": "/web-portfolio/static/media/Montserrat-Black.27e3649b.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/web-portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "1131be96908f79e232a0955db559192d",
+    "url": "/web-portfolio/static/media/Paso-1-Desktop.1131be96.png"
+  },
+  {
+    "revision": "6df9fd06149646550107df28ed42248a",
+    "url": "/web-portfolio/static/media/Paso-1-Mobile.6df9fd06.png"
+  },
+  {
+    "revision": "329bab1e7295fbca664c168382dc6073",
+    "url": "/web-portfolio/static/media/Paso-2-Desktop.329bab1e.png"
+  },
+  {
+    "revision": "224865d00079baccd4ca9759f35b985d",
+    "url": "/web-portfolio/static/media/Paso-3-Desktop.224865d0.png"
+  },
+  {
+    "revision": "13d1a5d546109d0b49082663c9e068f6",
+    "url": "/web-portfolio/static/media/Paso-3-Mobile.13d1a5d5.png"
+  },
+  {
+    "revision": "8af8d434e51cfc84eaf53134283a770e",
+    "url": "/web-portfolio/static/media/Reportes-Desktop-1.8af8d434.png"
+  },
+  {
+    "revision": "a534805ed6652ba649a072a043ccf4ca",
+    "url": "/web-portfolio/static/media/Reportes-Mobile-1.a534805e.png"
+  },
+  {
+    "revision": "d390394d7ea8f136ea46b63ff75de7e7",
+    "url": "/web-portfolio/static/media/Reportes-Mobile-2.d390394d.png"
+  },
+  {
+    "revision": "695d5ea4ba62587e5e73d4e7e953930a",
+    "url": "/web-portfolio/static/media/landing-01-desktop.695d5ea4.png"
+  },
+  {
+    "revision": "efebcad15663186f5a9c4248d71c5445",
+    "url": "/web-portfolio/static/media/landing-01-mobile.efebcad1.png"
+  },
+  {
+    "revision": "b793bc6cf9170049c1f2784a1d139620",
+    "url": "/web-portfolio/static/media/landing-02-desktop.b793bc6c.png"
+  },
+  {
+    "revision": "13005734b1e2b4270ba4ab3c31ead5a6",
+    "url": "/web-portfolio/static/media/landing-02-mobile.13005734.png"
+  },
+  {
+    "revision": "f90c26a1f0c9fd5375f0c46494801aa3",
+    "url": "/web-portfolio/static/media/landing-03-desktop.f90c26a1.png"
+  },
+  {
+    "revision": "5f9369630c78a1228a9ae74a3796d772",
+    "url": "/web-portfolio/static/media/profile_picture.5f936963.jpg"
+  }
+]);
